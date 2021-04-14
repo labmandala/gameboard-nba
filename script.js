3e526c5f20d5ff77465706d4ab7517d3af4f7b96
sarah <skg@neofilm.us>
2021-04-13 21:57:11 -0400

    Attach handler to toggle corresponding share-menu.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var main = function() {
     $('.more-btn').on('click', (event) => {
         $(event.currentTarget).siblings('.more-menu').toggle();
-    })
+    });
+    $('.share').on('click', (event) => {
+        $(event.currentTarget).next().toggle();
+    });
 };
 
 $(document).ready(main);
